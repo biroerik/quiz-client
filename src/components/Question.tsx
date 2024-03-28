b3d85e97e28451ef94bc73b850e19c37795b772f
Bíró Erik <biro.erik00@gmail.com>
2024-03-28 13:39:55 +0100

    fix

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,4 @@
-import { LinearProgress, Button, Box } from "@mui/material";
+import { LinearProgress, Button, Box, Grid } from "@mui/material";
 interface IProps {
   question: { question: string; answer: boolean };
   next: (answer: boolean) => void;
@@ -7,17 +7,21 @@ interface IProps {
 }
 const Question = ({ question, next, activeQuestion, quiz }: IProps) => {
   return (
-    <Box sx={{ p: 2, border: "2px red solid" }}>
+    <Grid>
       <h1>
         <LinearProgress
           variant="determinate"
           value={(100 / quiz.length) * (activeQuestion + 1)}
         />
       </h1>
-      <h2>{question.question}</h2>
-      <Button onClick={() => next(true)}>True</Button>
-      <Button onClick={() => next(false)}>False</Button>
-    </Box>
+      <Box m={2} sx={{ p: 2, border: "2px red solid" }}>
+        <h2>{question.question}</h2>
+      </Box>
+      <Grid item container justifyContent="space-evenly">
+        <Button onClick={() => next(true)}>True</Button>
+        <Button onClick={() => next(false)}>False</Button>
+      </Grid>
+    </Grid>
   );
 };
 
